bfa8867b1df690470d825ccb3f843e7aefbc3335
Diksha Gupta <simrandgupta95@gmail.com>
2026-06-05 13:57:04 +0530

    feat: add user auth (register, login, JWT, bcrypt)

---
+++ Backend/src/db/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+async function connectDb() {
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("connectd to MongoDB")
+
+    } catch (error) {
+        console.log("Error connecting to MongoDB:", err)
+    }
+}
+
+module.exports  = connectDb;
